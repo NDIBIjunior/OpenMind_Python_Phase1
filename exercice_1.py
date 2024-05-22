bd2d3e48171343d318bb357604748ffbcc90526f
NDIBIjunior <junioreyetemou@gmail.com>
2024-05-22 22:05:32 +0100

    Correction après la review de Mr Divad

--- exercice_1.py
+++ exercice_1.py
@@ -1,7 +1,3 @@
-#exercice 1  programme affichant un message de bienvenu avec le nom de l'utilisateur
-
-
 name = input("Bonjour!\nVeuillez entrer votre nom SVP\n")
+
 print("Bienvenu(e) Mr(Mme): ", name,"!!")
-
-
