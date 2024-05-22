d676d78e8c154e2a801057a13eca3fb40e272475
NDIBIjunior <junioreyetemou@gmail.com>
2024-05-22 04:29:09 +0100

    programme affichant un message de bienvenu avec le nom de l'utilisateur

---
+++ exercice_1.py
@@ -0,0 +1,7 @@
+#exercice 1  programme affichant un message de bienvenu avec le nom de l'utilisateur
+
+
+name = input("Bonjour!\nVeuillez entrer votre nom SVP\n")
+print("Bienvenu(e) Mr(Mme): ", name,"!!")
+
+
