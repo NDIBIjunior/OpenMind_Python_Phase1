65f848a651d28eab817e55b6d01a0ac18d0a3776
NDIBIjunior <junioreyetemou@gmail.com>
2024-05-23 01:30:48 +0100

    Exercice 2 version complète avec les bon formats

--- exercice_2.py
+++ exercice_2.py
@@ -1,7 +1,5 @@
+premier_nombre = float(input("Bonjour \nVeuillez entrer le premier nombre: "))
+second_nombre = float(input("entrer le second: "))
 
-premier_nombre = input("Bonjour \nVeuillez entrer le premier nombre: ")
-second_nombre = input("entrer le second: ")
-premier_nombre = float(premier_nombre)
-second_nombre = float(second_nombre)
 somme = premier_nombre + second_nombre
 print("la somme est: ", somme)
