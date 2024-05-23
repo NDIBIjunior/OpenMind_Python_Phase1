4df80c99bc3e3471525a80d2afb5b2a5e0a0f41d
NDIBIjunior <junioreyetemou@gmail.com>
2024-05-23 01:58:46 +0100

    Modification de l'exercice 2 avec ajout des bons formats et la mise sur une ligne des différents CAST

--- exercice_2.py
+++ exercice_2.py
@@ -1,4 +1,5 @@
 premier_nombre = float(input("Bonjour \nVeuillez entrer le premier nombre: "))
 second_nombre = float(input("entrer le second: "))
+
 somme = premier_nombre + second_nombre
 print("la somme est: ", somme)
