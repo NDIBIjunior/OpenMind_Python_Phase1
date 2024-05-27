d5ea29af2e711d2676e7b613a8251e1d2bed1c4c
NDIBIjunior <junioreyetemou@gmail.com>
2024-05-27 04:15:56 +0100

    Programme calculant la surface et le périmètre d'un cercle à partir de son rayon

---
+++ exercice_10.py
@@ -0,0 +1,5 @@
+rayon = int(input("Veuillez entrer le rayon du cercle: "))
+Surface = 3.14 * rayon*rayon
+perimetre = 3.14 * rayon
+
+print("Le perimètre de ce cercle est ", perimetre,"m\nEt l'aire est de ", Surface, "m²")
