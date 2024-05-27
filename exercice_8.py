36e687325ea2da5824464636770ae4a5f0737fdc
NDIBIjunior <junioreyetemou@gmail.com>
2024-05-27 04:05:43 +0100

    Programme donnant la somme des n premiers entiers

---
+++ exercice_8.py
@@ -0,0 +1,6 @@
+nombre = int(input("Veuillez entrer un nombre:"))
+somme = int(0)
+for i in range(nombre +1):
+    somme = somme + i
+
+print("La somme de 0 jusqu'à ", nombre, "est egale à ", somme)
