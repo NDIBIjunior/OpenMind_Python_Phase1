088b9eff65d097fd8f1c6d8057c78759906e3ae9
NDIBIjunior <junioreyetemou@gmail.com>
2024-05-27 05:48:06 +0100

    Ajout du programme permettant de détecter si une liste ou une chaine est vide

---
+++ exercice_28.py
@@ -0,0 +1,13 @@
+liste = list(input("Entrer les éléments de votre liste "))
+if(liste == []):
+    print("Votre liste est vide !!!")
+else:
+    print("Votre liste comporte des éléments")
+
+chaine = input("Entrer votre chaine de caractère: ")
+
+if(chaine == ""):
+    print("Votre chaine est vide !!")
+else:
+    print("Votre chaine comporte des caractères ")
+
