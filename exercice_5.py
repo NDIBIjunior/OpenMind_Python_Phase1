21dddac5f0461cfe2ef3829aff0af308f43cc85f
NDIBIjunior <junioreyetemou@gmail.com>
2024-05-27 02:08:31 +0100

    Modification du programme permettant de déterminer la parité d'un nombre: Suppression des parenthèses du if

--- exercice_5.py
+++ exercice_5.py
@@ -1,5 +1,5 @@
 nombre = float(input("Entrer un nombre: "))
-if (nombre % 2 == 0):
+if nombre % 2 == 0:
     print("le nombre", nombre," est pair !!!")
 else:
     print("Le nombre", nombre, "est impair !!!")
