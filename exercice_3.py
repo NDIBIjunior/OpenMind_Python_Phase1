2a3f017444b75ab58562231bd0ed11098f972a97
NDIBIjunior <junioreyetemou@gmail.com>
2024-05-27 01:14:36 +0100

    Ajout de l'exercice 3 qui détermine le plus grand de deux nombres

--- exercice_3.py
+++ exercice_3.py
@@ -1,4 +1,5 @@
 premier_nombre = float(input("Bonjour \nVeuillez entrer le premier nombre: "))
+
 second_nombre = float(input("entrer le second: "))
 if premier_nombre < second_nombre:
     print("Le plus grand est: ", second_nombre)
